7bf896edc99d81e8e96e0d225878063f4ce6a64b
Scott Hodson <sirmushroompanda@hotmal.co.uk>
2013-05-23 11:16:36 +0100

    Let's Mod with Pahimar Ep5 Complete.

---
+++ Mine-Bits common/sirmp/minebits/MineBits.java
@@ -0,0 +1,29 @@
+package sirmp.minebits;
+
+import sirmp.minebits.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod( modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class MineBits {
+    
+    @PreInit
+    public void PreInit(FMLPreInitializationEvent event) {
+        
+    }
+    
+    @Init
+    public void Init(FMLInitializationEvent event) {
+        
+    }
+    
+    @PostInit
+    public void PostInit(FMLPostInitializationEvent event) {
+        
+    }
+}
---
+++ Mine-Bits common/sirmp/minebits/lib/Reference.java
@@ -0,0 +1,8 @@
+package sirmp.minebits.lib;
+
+public class Reference {
+
+    public static final String MOD_ID = "MBs";
+    public static final String MOD_NAME = "Mine-Bits";
+    public static final String VERSION = "0.0.1";
+}
